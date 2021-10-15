ceabef632744df1bb1673276d650bbbad23b9bcf
alexzhelyapov1 <55490812+alexzhelyapov1@users.noreply.github.com>
2021-10-15 14:40:39 +0300

    Add files via upload

---
+++ Matrix_Multiply.cpp
@@ -0,0 +1,65 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void ScanMatrix (int *matrix, int a, int b);
+void PrintMatrix (int *matrix, int a, int b);
+void MultiplyMatrix (int *first_matrix, int *second_matrix, int *result_matrix,	int a, int b, int y);
+
+
+int main () {
+	int a, b, x, y; // first matrix a*b, second x*y
+	scanf ("%d %d %d %d", &a, &b, &x, &y);
+	if (b != x) {
+		printf ("-1");
+		return 0;
+	}
+	int *first_matrix = (int *) calloc (a * b, sizeof (int));
+	int *second_matrix = (int *) calloc (x * y, sizeof (int));
+	int *result_matrix = (int *) calloc (a * y, sizeof (int));
+
+	ScanMatrix (first_matrix, a, b);
+	//PrintMatrix (first_matrix, a, b);
+	ScanMatrix (second_matrix, x, y);
+	//PrintMatrix (second_matrix, x, y);
+	MultiplyMatrix (first_matrix, second_matrix, result_matrix, a, b, y);
+	PrintMatrix (result_matrix, a, y);
+
+
+	free (first_matrix);
+	free (second_matrix);
+	free (result_matrix);
+	return 0;
+}
+
+
+void ScanMatrix (int *matrix, int a, int b) {
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			scanf ("%d", &matrix[i * b + j]);
+		}
+	}
+}
+
+
+void PrintMatrix (int *matrix, int a, int b) {
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < b; j++) {
+			printf ("%d ", matrix[i * b + j]);
+		}
+		printf ("\n");
+	}
+}
+
+
+void MultiplyMatrix (int *first_matrix, int *second_matrix, int *result_matrix,	int a, int b, int y) {
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < y; j++) {
+			int sum = 0;
+			for (int k = 0; k < b; k++) {
+				sum += first_matrix[i * b + k] * second_matrix[k * y + j];
+			}
+			result_matrix[i * y + j] = sum;
+		}
+	}
+}
